4ff592e23657bba9926203da5719152305f78327
Sachin <raisethebat2020@gmail.com>
2024-07-12 11:58:19 +0530

    developed search bar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,12 +1,22 @@
+import { BsSearch } from "react-icons/bs";
 import AdminSidebar from "../components/AdminSidebar";
+import userImg from "../assets/userpic.png";
+import { FaRegBell } from "react-icons/fa";
 
 const Dashboard = () => {
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main>
-        hi
-      </main>
+
+        <main className="dashboard">
+          <div className="bar">
+              <BsSearch/>
+              <input type="text" placeholder="Search for data, users, docs" />
+              <FaRegBell/>
+              <img src={userImg} alt="User" />
+          </div>
+        </main>
+
     </div>
   );
 };
